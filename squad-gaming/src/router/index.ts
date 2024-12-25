eb1ecf9debc234eabb5d45755ca6c75ba52386f2
Alexander Meinhard <alexander@meinhard.de>
2024-12-25 13:35:50 +0100

    Router change

--- squad-gaming/src/router/index.ts
+++ squad-gaming/src/router/index.ts
@@ -9,7 +9,7 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory('/squad-gaming/'), //import.meta.env.BASE_URL
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
